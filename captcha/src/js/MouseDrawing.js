f9a293fc9f139b245df1e8a3780e784ddf0db567
rfrye <rfrye09@gmail.com>
2025-06-13 01:27:30 -0400

    initial dev. setup basic flow.

---
+++ captcha/src/js/MouseDrawing.js
@@ -0,0 +1,48 @@
+﻿const canvas = document.getElementById('drawCanvas');
+const ctx = canvas.getContext('2d');
+let isMouseDrawing = false;
+
+function getMousePos(event) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
+
+//Potentially evaluate density of drawing, require a minimum threshold
+//Display how close they are on mouseup
+function getDrawingDensity() {
+    const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    let nonTransparentPixels = 0;
+    for (let i = 3; i < pixels.length; i += 4) {
+        if (pixels[i] !== 0) nonTransparentPixels++;
+    }
+
+    const totalPixels = canvas.width * canvas.height;
+    return nonTransparentPixels / totalPixels; // fraction of canvas used
+}
+
+canvas.addEventListener('mousedown', (event) => {
+    isMouseDrawing = true;
+    ctx.beginPath();
+    const pos = getMousePos(event);
+    ctx.moveTo(pos.x, pos.y);
+});
+
+canvas.addEventListener('mousemove', (event) => {
+    if (isMouseDrawing) {
+        const pos = getMousePos(event);
+        ctx.lineTo(pos.x, pos.y);
+        ctx.stroke();
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    isMouseDrawing = false;
+});
+
+canvas.addEventListener('mouseleave', () => {
+    isMouseDrawing = false;
+});
---
+++ captcha/src/js/TouchDrawing.js
@@ -0,0 +1,33 @@
+﻿const canvasTouch = document.getElementById('drawCanvas');
+const ctxTouch = canvasTouch.getContext('2d');
+let isDrawingTouch = false;
+
+function getPos(e) {
+    const rect = canvasTouch.getBoundingClientRect();
+    const touch = e.touches ? e.touches[0] : e;
+    return {
+        x: touch.clientX - rect.left,
+        y: touch.clientY - rect.top
+    };
+}
+
+canvasTouch.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    isDrawingTouch = true;
+    ctxTouch.beginPath();
+    const pos = getPos(e);
+    ctxTouch.moveTo(pos.x, pos.y);
+});
+
+canvasTouch.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    if (isDrawingTouch) {
+        const pos = getPos(e);
+        ctxTouch.lineTo(pos.x, pos.y);
+        ctxTouch.stroke();
+    }
+});
+
+canvasTouch.addEventListener('touchend', () => {
+    isDrawingTouch = false;
+});
