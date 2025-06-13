b3c0d67597b5a99514d9e88d187595310b252236
rfrye <rfrye09@gmail.com>
2025-06-13 09:33:44 -0400

    cleanup

---
+++ captcha/src/js/Config.js
@@ -0,0 +1,24 @@
+﻿const noOfDrawings = 9;
+const promptWords = [
+    "cat", 
+    "dog", 
+    "fox", 
+    "horse",
+    "elephant",
+    "giraffe",
+    "lion",
+    "tiger",
+    "bear",
+    "wolf",
+    "zebra",
+    "panda",
+    "rabbit",
+    "squirrel",
+    "racoon",
+    "possum",
+    "donkey",
+    "platypus",
+    "duck",
+    "chicken",
+    "goose"
+];
